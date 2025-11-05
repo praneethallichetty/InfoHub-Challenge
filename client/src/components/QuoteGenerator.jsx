6ca07b4807b6306e213f4f8113090ff0454c81ca
praneethallichetty <134947500+praneethallichetty@users.noreply.github.com>
2025-11-05 23:21:19 +0530

    Update API endpoint for fetching quotes

--- client/src/components/QuoteGenerator.jsx
+++ client/src/components/QuoteGenerator.jsx
@@ -1,3 +1,30 @@
+// import React, { useState } from "react";
+// import axios from "axios";
+
+// function QuoteGenerator() {
+//   const [quote, setQuote] = useState("");
+
+//   const fetchQuote = async () => {
+//     try {
+//       const response = await axios.get("http://localhost:3001/api/quote");
+//       setQuote(response.data.quote);
+//     } catch (error) {
+//       console.error("Error fetching quote:", error);
+//     }
+//   };
+
+//   return (
+//     <div className="card">
+//       <h2>💬 Quote Generator</h2>
+//       <button onClick={fetchQuote}>Get a Quote</button>
+//       {quote && <p className="result">"{quote}"</p>}
+//     </div>
+//   );
+// }
+
+// export default QuoteGenerator;
+
+
 import React, { useState } from "react";
 import axios from "axios";
 
@@ -6,7 +33,9 @@ function QuoteGenerator() {
 
   const fetchQuote = async () => {
     try {
-      const response = await axios.get("http://localhost:3001/api/quote");
+      const response = await axios.get(
+        "https://info-hub-challenge-ten.vercel.app/api/quote"
+      );
       setQuote(response.data.quote);
     } catch (error) {
       console.error("Error fetching quote:", error);
